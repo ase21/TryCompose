ae5415bde57436d1defac5ec90092d470073dd35
ase911 <ase911@yandex.ru>
2023-10-10 11:33:33 +0300

    google codelabs 11 complete

--- app/src/main/java/com/asefactory/trycompose/MainActivity.kt
+++ app/src/main/java/com/asefactory/trycompose/MainActivity.kt
@@ -27,6 +27,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -52,7 +53,7 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun MyApp(){
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
     Surface(modifier = Modifier.fillMaxSize()) {
         if (shouldShowOnboarding){
             OnboardingScreen { shouldShowOnboarding = false }
@@ -107,8 +108,6 @@ fun MessageCard(message: Message, modifier: Modifier = Modifier) {
         val surfaceColor by animateColorAsState(
             if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
         )
-
-
 
         Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
--- app/src/main/java/com/asefactory/trycompose/ui/composables/greetings.kt
+++ app/src/main/java/com/asefactory/trycompose/ui/composables/greetings.kt
@@ -1,5 +1,8 @@
 package com.asefactory.trycompose.ui.composables
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -8,35 +11,43 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-
 @Composable
 fun Greetings(name: String) {
 
-    val isExpanded = remember {
-        mutableStateOf(false)
-    }
+    var isExpanded by rememberSaveable { mutableStateOf(false) }
 
-    val extraPadding = if (isExpanded.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        targetValue = if (isExpanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f).padding(bottom = extraPadding)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
             ElevatedButton(onClick = {
-                isExpanded.value = !isExpanded.value
+                isExpanded = !isExpanded
             }) {
-                Text(text = if(isExpanded.value) "Some less" else "Some more")
+                Text(text = if(isExpanded) "Some less" else "Some more")
             }
         }
     }
